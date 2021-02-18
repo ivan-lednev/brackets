af23cff528e2d4f544fbdaa2bf54b868e444a4ec
Ivan Liadniou <ivan_liadniou@epam.com>
2021-02-18 11:05:37 +0100

    Add bracket matcher

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let brackets = str.split("");
+    function testMatchingBrackets(left, right) {
+        return bracketsConfig.some(
+            (pair) => pair[0] === left && pair[1] === right
+        );
+    }
+
+    function removeNeighbors() {
+        for (let i = brackets.length - 2; i >= 0; i--) {
+            const current = brackets[i];
+            const next = brackets[i + 1];
+            if (testMatchingBrackets(current, next)) {
+                brackets.splice(i, 2);
+            }
+        }
+    }
+
+    let previousLength = brackets.length;
+    do {
+        previousLength = brackets.length;
+        removeNeighbors();
+    } while (brackets.length > 0 && brackets.length < previousLength);
+
+    return brackets.length === 0;
+};
